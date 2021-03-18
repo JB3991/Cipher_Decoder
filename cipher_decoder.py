60a3cd4cf61e91733afb00b62f9beac2d7ee22c6
Jon Burnett <74561172+JB3991@users.noreply.github.com>
2021-03-18 21:15:02 +0000

    Initial commit

---
+++ cipher_decoder.py
@@ -0,0 +1,35 @@
+import art
+print(art.logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(plain_text, shift_amount, cipher_direction):
+  cipher_text = ""
+
+  if cipher_direction == "decode":
+      shift_amount *= -1
+
+  for char in plain_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      cipher_text += alphabet[new_position] 
+    else: 
+      cipher_text += char
+
+  print(f"The {cipher_direction} text is {cipher_text}")
+
+should_continue = True
+while should_continue:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  shift = shift % 26
+  caesar(plain_text=text, shift_amount=shift, cipher_direction=direction)
+
+  result = input("Type 'yes' if you want to go again. Otherwise typoe 'no'. \n")
+
+  if result == "no":
+    should_continue = False
+    print("Goodbye")
